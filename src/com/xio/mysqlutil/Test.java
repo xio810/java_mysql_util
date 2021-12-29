82144a67cf65e6d77951360a2597cc053cb39b97
xio810 <boi4806@naver.com>
2021-12-29 12:00:56 +0900

    DB 연결, 쿼리결과 출력

--- src/com/xio/mysqlutil/Test.java
+++ src/com/xio/mysqlutil/Test.java
@@ -1,7 +1,74 @@
 package com.xio.mysqlutil;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Test {
+
+	// JDBC driver name and database URL
+	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/mysqlutil?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull&connectTimeout=60";
+
+	// Database credentials
+	static final String USER = "xio810";
+	static final String PASS = "rlaalsgus2";
+
 	public static void main(String[] args) {
-		System.out.println("안녕");
-	}
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			// STEP 2: Register JDBC driver
+			Class.forName(JDBC_DRIVER);
+
+			// STEP 3: Open a connection
+			System.out.println("Connecting to a selected database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			System.out.println("Connected database successfully...");
+
+			// STEP 4: Execute a query
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+
+			String sql = "SELECT * FROM article";
+			ResultSet rs = stmt.executeQuery(sql);
+			// STEP 5: Extract data from result set
+			while (rs.next()) {
+				// Retrieve by column name
+				int id = rs.getInt("id");
+				String regDate = rs.getString("regDate");
+				String title = rs.getString("title");
+				String body = rs.getString("body");
+
+				// Display values
+				System.out.print("id: " + id);
+				System.out.print(", regDate: " + regDate);
+				System.out.print(", title: " + title);
+				System.out.println(", body: " + body);
+			}
+			rs.close();
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					conn.close();
+			} catch (SQLException se) {
+			} // do nothing
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			} // end finally try
+		} // end try
+		System.out.println("Goodbye!");
+	}// end main
 }

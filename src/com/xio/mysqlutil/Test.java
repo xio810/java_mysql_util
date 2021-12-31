495c8e61b61bc2f3993cc4a2f10516475e87a530
xio810 <boi4806@naver.com>
2021-12-31 19:02:19 +0900

    mysqlutil 변경 및 테스트

--- src/com/xio/mysqlutil/Test.java
+++ src/com/xio/mysqlutil/Test.java
@@ -8,34 +8,73 @@ public class Test {
 		MysqlUtil.setDBInfo("localhost", "sbsst", "sbs123414", "mysqlutil");
 
 		MysqlUtil.setDevMode(true);
-
-		List<Map<String, Object>> articleListMap = MysqlUtil.selectRows(new SecSql().append("SELECT * FROM article"));
+		// select rows
+		SecSql sql1 = new SecSql();
+		sql1.append("SELECT * FROM article order by id desc");
+		List<Map<String, Object>> articleListMap = MysqlUtil.selectRows(sql1);
 		System.out.println("articleListMap : " + articleListMap);
 
-		Map<String, Object> articleMap = MysqlUtil
-				.selectRow(new SecSql().append("SELECT * FROM article WHERE id = ?", 1));
+		// select row
+		SecSql sql2 = new SecSql();
+		sql2.append("SELECT * FROM article WHERE id = ?", 1);
+		Map<String, Object> articleMap = MysqlUtil.selectRow(sql2);
 		System.out.println("articleMap : " + articleMap);
 
-		String title = MysqlUtil.selectRowStringValue(new SecSql().append("SELECT title FROM article WHERE id = ?", 1));
+		// select row string value
+		SecSql sql3 = new SecSql();
+		sql3.append("SELECT title FROM article WHERE id = ?", 1);
+		String title = MysqlUtil.selectRowStringValue(sql3);
 		System.out.println("title : " + title);
 
-		int id = MysqlUtil.selectRowIntValue(new SecSql().append("SELECT id FROM article WHERE id = ?", 1));
+		// select row int value
+		SecSql sql4 = new SecSql();
+		sql4.append("SELECT id FROM article WHERE id = ?", 1);
+		int id = MysqlUtil.selectRowIntValue(sql4);
 		System.out.println("memberId : " + id);
 
-		boolean idIs1 = MysqlUtil
-				.selectRowBooleanValue(new SecSql().append("SELECT id = 1 FROM article WHERE id = ?", 1));
+		// select row boolean value
+		SecSql sql5 = new SecSql();
+		sql5.append("SELECT id = 1 FROM article WHERE id = ?", 1);
+		boolean idIs1 = MysqlUtil.selectRowBooleanValue(sql5);
 		System.out.println("id is 1 : " + idIs1);
 
+		// insert
 		String newTitle = "새 제목";
 		String newBody = "새 내용";
 
-		SecSql sql = new SecSql().append("INSERT INTO article");
-		sql.append("SET regDate = NOW()");
-		sql.append(", updateDate = NOW()");
-		sql.append(", title = ?", newTitle);
-		sql.append(", body = ?", newBody);
+		SecSql sql6 = new SecSql();
+		sql6.append("INSERT INTO article");
+		sql6.append("SET regDate = NOW()");
+		sql6.append(", updateDate = NOW()");
+		sql6.append(", title = ?", newTitle);
+		sql6.append(", body = ?", newBody);
+		int newArticleId = MysqlUtil.insert(sql6);
+		System.out.println("newArticleId" + newArticleId);
 
-		MysqlUtil.insert(sql);
+		// update
+		SecSql sql7 = new SecSql();
+		sql7.append("update article");
+		sql7.append("set updateDate = now()");
+		sql7.append(", title = CONCAT(title, '_NEW')");
+		sql7.append("WHERE id = ?", 3);
+		MysqlUtil.update(sql7);
+
+		// select title id 3
+		SecSql sql8 = new SecSql();
+		sql8.append("select title from article where id = ?", 3);
+		String article3__title = MysqlUtil.selectRowStringValue(sql8);
+		System.out.println("article3__title : " + article3__title);
+
+		// delete
+		SecSql sql9 = new SecSql();
+		sql9.append("delete from article where id = ?", newArticleId);
+		MysqlUtil.delete(sql9);
+
+		// count
+		SecSql sql10 = new SecSql();
+		sql10.append("select count(*) as cnt from article where id = ?", newArticleId);
+		int count = MysqlUtil.selectRowIntValue(sql10);
+		System.out.println("count : " + count);
 
 		MysqlUtil.closeConnection();
 	}
